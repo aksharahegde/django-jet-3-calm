1f66cb84267f28970521e1a8a239da10b3050034
aksharahegde <akshara@blowhorn.com>
2022-02-10 00:09:57 +0530

    build(jet) added latest build

---
+++ build/lib/jet/ordered_set.py
@@ -0,0 +1,72 @@
+try:
+    from collections import MutableSet
+except ImportError: # Python 3.3+
+    '''
+    MutableSet is now in collections.abc. Starting with Python 3.3, 
+    importing ABCs from collections was deprecated, and importing 
+    should be done from collections.abc. Being able to import from 
+    collections was marked for removal in 3.8, but has been delayed 
+    to 3.9.
+
+    Taken from:
+    https://github.com/OCA/oca-addons-repo-template/issues/80#issuecomment-948531410
+    '''
+    from collections.abc import MutableSet
+
+#class OrderedSet(collections.MutableSet):
+class OrderedSet(MutableSet):
+    def __init__(self, iterable=None):
+        self.end = end = []
+        end += [None, end, end]         # sentinel node for doubly linked list
+        self.map = {}                   # key --> [key, prev, next]
+        if iterable is not None:
+            self |= iterable
+
+    def __len__(self):
+        return len(self.map)
+
+    def __contains__(self, key):
+        return key in self.map
+
+    def add(self, key):
+        if key not in self.map:
+            end = self.end
+            curr = end[1]
+            curr[2] = end[1] = self.map[key] = [key, curr, end]
+
+    def discard(self, key):
+        if key in self.map:
+            key, prev, next = self.map.pop(key)
+            prev[2] = next
+            next[1] = prev
+
+    def __iter__(self):
+        end = self.end
+        curr = end[2]
+        while curr is not end:
+            yield curr[0]
+            curr = curr[2]
+
+    def __reversed__(self):
+        end = self.end
+        curr = end[1]
+        while curr is not end:
+            yield curr[0]
+            curr = curr[1]
+
+    def pop(self, last=True):
+        if not self:
+            raise KeyError('set is empty')
+        key = self.end[1][0] if last else self.end[2][0]
+        self.discard(key)
+        return key
+
+    def __repr__(self):
+        if not self:
+            return '%s()' % (self.__class__.__name__,)
+        return '%s(%r)' % (self.__class__.__name__, list(self))
+
+    def __eq__(self, other):
+        if isinstance(other, OrderedSet):
+            return len(self) == len(other) and list(self) == list(other)
+        return set(self) == set(other)
