f5ce52a90066ffe2ff12703a5277d62bbb869993
Akshara Hegde <akshara@blowhorn.com>
2022-09-16 20:15:01 +0530

    feat - trigger native event on select2 change

--- jet/__init__.py
+++ jet/__init__.py
@@ -1,1 +1,2 @@
-VERSION = '5.1.1'
+# VERSION = '5.1.1'
+VERSION = '5.1.2'
--- jet/static/jet/js/src/features/selects.js
+++ jet/static/jet/js/src/features/selects.js
@@ -124,15 +124,14 @@ Select2.prototype = {
             var $element = this.$element;
             $element.on('change', function (e) {
                 console.log('Select change triggered... 1');
-                $element.trigger('change');
 
                 // Hack to get native event to allow addEventListener
-                // var tempValStore = document.createElement("input");
-                // tempValStore.name = $element.name;
-                // e.target.appendChild(tempValStore)
-                // tempValStore.value = e.target.value;
-                // // tempValStore.dispatchEvent(new Event("change"))
-                // e.target.removeChild(tempValStore)
+                var tempValStore = document.createElement("input");
+                tempValStore.name = $element.name;
+                e.target.appendChild(tempValStore)
+                tempValStore.value = e.target.value;
+                tempValStore.dispatchEvent(new Event("change"))
+                e.target.removeChild(tempValStore)
             });
 
             $dropdown.find('.select2-buttons-button-select-all').on('click', function (e) {
